cb83492d03ec891911e0ea0f2d9412da8f59737a
evanpy <evanpurnama5@gmail.com>
2012-10-10 23:59:17 +0800

    'a'

---
+++ ip.rb
@@ -0,0 +1,21 @@
+tmd = 0
+while 1
+  t = Time.now
+  tm = t.sec
+  if tm%10 == 0
+  	tmo = tm
+	if tmd == 0
+		puts Time.now
+		`ipconfig > ip.txt`
+		`git add .`
+		`git commit -m 'a'`
+		`git push origin master`
+	end
+  	tmd = 1
+  end
+  if !tmo.nil?
+	  if tm == tmo + 5
+		tmd = 0
+	  end
+  end
+end
